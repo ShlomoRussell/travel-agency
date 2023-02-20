33aa284f7fcd20958958882eef796d7a0c87ef9e
ShlomoRussell <93613042+ShlomoRussell@users.noreply.github.com>
2023-02-20 19:34:53 +0200

    moved services to folder

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,14 +2,16 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
-import { AddVacationComponent } from './add-vacation/add-vacation.component';
+import { AddVacationComponent } from './components/add-vacation/add-vacation.component';
 import { HttpClientModule } from '@angular/common/http';
-import { VacationsComponent } from './vacations/vacations.component';
+import { VacationsComponent } from './components/vacations/vacations.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { MatAutocompleteModule } from '@angular/material/autocomplete';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatInputModule } from '@angular/material/input';
+import { MatDatepickerModule } from '@angular/material/datepicker';
+import { MatNativeDateModule } from '@angular/material/core';
 
 @NgModule({
   declarations: [AppComponent, AddVacationComponent, VacationsComponent],
@@ -22,6 +24,8 @@ import { MatInputModule } from '@angular/material/input';
     ReactiveFormsModule,
     MatFormFieldModule,
     MatInputModule,
+    MatDatepickerModule,
+    MatNativeDateModule,
   ],
   providers: [],
   bootstrap: [AppComponent],
